01249a1ed6ee5cd8acf9d5e0b6b0c972ad4ecb81
ursnaksh <er.nageshs@gmail.com>
2026-09-24 05:03:43 +0530

    Load FLUX RIDE 2026 theme

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './styles/index.css';
+import './styles/flux-v2.css';
 
 // Note: React 18 StrictMode intentionally double-invokes effects/renders in
 // development to help surface side-effect bugs. This is expected behavior,
