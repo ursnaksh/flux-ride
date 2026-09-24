4a7d813d6e13ca33b9d3cdc6092001c6bdb21f1e
ursnaksh <er.nageshs@gmail.com>
2026-09-24 06:10:29 +0530

    Sync OTP verification backend

--- backend/src/main/java/com/flux/repository/UserRepository.java
+++ backend/src/main/java/com/flux/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.flux.repository;
 import com.flux.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByPhone(String phone);
+    Optional<User> findFirstByPhoneIn(Collection<String> phones);
     boolean existsByPhone(String phone);
 }
